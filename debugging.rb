c5fe6868261e8d8030dd2f20c36a04cf0ab320f7
debbie <drosenfeld87@gmail.com>
2018-02-24 10:23:53 -0500

    complete exercise

--- debugging.rb
+++ debugging.rb
@@ -1,13 +1,16 @@
 def select_cards(possible_cards, hand)
 
+
+
   possible_cards.each do |current_card|
     print "Do you want to pick up #{current_card}?"
     answer = gets.chomp
     if answer.downcase == 'y'
       hand << current_card
     end
-    return hand
   end
+
+  return hand
 
 end
 
